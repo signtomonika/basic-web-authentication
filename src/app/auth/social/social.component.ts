3628914c22e9461174736137eb7c3dcb7d80ba71
Monika Ravi <monikar@hawsco.com>
2018-08-15 20:52:55 -0700

    Some cosmetic changes

--- src/app/auth/social/social.component.ts
+++ src/app/auth/social/social.component.ts
@@ -4,34 +4,45 @@ import { SocialService } from '../social.service';
 import * as hello from 'hellojs/dist/hello.all.js'
 import { MicrosoftService } from '../microsoft.service';
 
+
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
   styleUrls: ['./social.component.css']
+ 
 })
 export class SocialComponent implements OnInit {
 
-  constructor(private socialService : SocialService , private microsoft : MicrosoftService) { }
+  state = 'final';
+
+  start = false;
+
+  constructor(private socialService: SocialService, private microsoft: MicrosoftService) { }
 
   ngOnInit() {
+
+
+
   }
 
-  socialSign(provider : string) {
+  socialSign(provider: string) {
 
-  
-  
+
+
     this.socialService.setProvider(provider);
-    
- 
-  
+
+
+
   }
 
   windowsSign() {
 
     //console.log('Microsoft Logged In');
 
-    this.microsoft.login() ;
+    this.microsoft.login();
 
   }
+
+ 
 
 }
